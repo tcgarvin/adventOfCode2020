b45ab7ea549f6944fb31fae4e5b606ed7073d375
Tim Garvin <timothy.garvin@pwc.com>
2020-11-30 12:10:17 -0500

    Add solution template from last year

---
+++ template.py
@@ -0,0 +1,21 @@
+def solve_part_1(puzzle_input):
+    return ""
+
+def solve_part_2(puzzle_input):
+    return ""
+
+def get_puzzle_input():
+    puzzle_input = []
+    with open("input.txt") as input_txt:
+        for line in input_txt:
+            pass
+    return puzzle_input
+
+if __name__ == "__main__":
+    puzzle_input = get_puzzle_input()
+
+    answer_1 = solve_part_1(puzzle_input)
+    print(f"Part 1: {answer_1}")
+
+    answer_2 = solve_part_2(puzzle_input)
+    print(f"Part 2: {answer_2}")
