0e78d5489996573e2e81b57ca19af662bda7ca3b
Tim Garvin <tcgarvin@gmail.com>
2020-12-06 23:52:42 -0500

    Day 6

---
+++ 06/customs.py
@@ -0,0 +1,44 @@
+from functools import reduce
+from rich import print
+
+def solve_part_1(puzzle_input):
+    total = 0
+    for group in puzzle_input:
+        all_questions = reduce(lambda a,b: a | b, group)
+        total += len(all_questions)
+
+    return total
+
+
+def solve_part_2(puzzle_input):
+    total = 0
+
+    for group in puzzle_input:
+        all_questions = reduce(lambda a,b: a & b, group)
+        total += len(all_questions)
+
+    return total
+
+
+def get_puzzle_input():
+    puzzle_input = []
+    group = []
+    puzzle_input.append(group)
+    with open("input.txt") as input_txt:
+        for line in input_txt:
+            if len(line.strip()) == 0:
+                group = []
+                puzzle_input.append(group)
+            else:
+                group.append(set(line.strip()))
+
+    return puzzle_input
+
+if __name__ == "__main__":
+    puzzle_input = get_puzzle_input()
+
+    answer_1 = solve_part_1(puzzle_input)
+    print(f"Part 1: {answer_1}")
+
+    answer_2 = solve_part_2(puzzle_input)
+    print(f"Part 2: {answer_2}")
